d33607dfc1ef66e0e56236ea716604bb5016a2d1
qusai sappari <sappari.aq@gmail.com>
2025-03-18 21:54:25 +0800

    routing -1

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/users", (req, res) => {
+router.get("/", (req, res) => {
   const users = [
     { id: 1, name: "Alice" },
     { id: 2, name: "Bob" },
@@ -10,7 +10,7 @@ router.get("/users", (req, res) => {
   res.render("users", { users: users }); // You'll need to create users.ejs
 });
 
-router.get("/users/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const userId = req.params.id;
   res.send(`User ID: ${userId}`);
 });
